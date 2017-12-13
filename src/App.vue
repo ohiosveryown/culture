d1100cfa624afc70f9cca8a14423c78439069e08
ohiosveryown <matt@ohiosveryown.co>
2017-12-13 10:07:12 -0500

    Router Linked

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
+<!-- layout -->
 <template>
   <div id="culture">
-    <img class="mt-9" src="./assets/logo.png">
+
+    <!-- magic 🎩 happens below -->
     <router-view/>
+
   </div>
 </template>
 
+
+<!-- style -->
 <style lang="scss">
   @import './util/var.scss';
   @import './util/reset.scss';
@@ -14,17 +19,17 @@
 
   #culture {
     // font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: var(--sagan);
-    margin-top: 60px;
-    @include breakpoint(lg) { color: var(--gravity); }
+    // -webkit-font-smoothing: antialiased;
+    // -moz-osx-font-smoothing: grayscale;
+    // text-align: center;
+    // color: var(--sagan);
+    // margin-top: 60px;
+    // @include breakpoint(lg) { color: var(--gravity); }
   }
 </style>
 
+
+<!-- logic -->
 <script>
-  export default {
-    name: 'culture'
-  }
+  export default { name: 'culture' }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import list from '@/components/list'
+import tracey from '@/components/ppl/tracey'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'list',
+      component: list
+    },
+    {
+      path: '/tracey',
+      name: 'tracey',
+      component: tracey
     }
   ]
 })
