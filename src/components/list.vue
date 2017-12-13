e3acd385fb6243aba6ab522845669f19e62bc260
ohiosveryown <matt@ohiosveryown.co>
2017-12-13 10:07:34 -0500

    Router Link Working

---
+++ src/components/list.vue
@@ -0,0 +1,31 @@
+<!-- layout -->
+<template>
+  <div class="hello">
+
+
+    <router-link :to="{ name: 'tracey' }">
+      <div class="box"/>
+    </router-link>
+    <!-- <router-link @click="trigger = !trigger" :to="{ name: 'feb' }">View Entry</router-link> -->
+
+  </div>
+</template>
+
+
+<!-- style -->
+<style lang="scss" scoped>
+  @import '../util/grid.scss';
+
+  .box {
+    background: pink;
+    width: 400px; height: 400px;
+  }
+
+</style>
+
+<!-- logic -->
+<script>
+  export default {
+    name: 'list',
+  }
+</script>
---
+++ src/components/ppl/tracey.vue
@@ -0,0 +1,27 @@
+<!-- layout -->
+<template>
+  <div class="hello">
+
+    <h1>Tracey</h1>
+
+  </div>
+</template>
+
+
+<!-- style -->
+<style lang="scss" scoped>
+  @import '../../util/grid.scss';
+
+  .box {
+    background: pink;
+    width: 400px; height: 400px;
+  }
+
+</style>
+
+<!-- logic -->
+<script>
+  export default {
+    name: 'tracey',
+  }
+</script>
